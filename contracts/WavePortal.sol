bf159b114ed2b8bc24ccb9e492164c3a3eea8e28
mcgodamn <t7214948ttt@gmail.com>
2022-01-03 02:11:28 +0800

    update contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,17 +7,35 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
     constructor() {
         console.log("I am a fking smart contract.");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
     }
-
+    
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+    
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves", totalWaves);
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
+        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -8,16 +8,16 @@ const main = async () => {
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber());
 
-    let waveTxn = await waveContract.wave();
+    let waveTxn = await waveContract.wave("A message!");
     await waveTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves();
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
     await waveTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves();
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
 
 const runMain = async() => {
